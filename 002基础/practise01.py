21ddf94ef543615eaf863c487aadf20c7bf39ac0
Chao Ming <sxmc1994@163.com>
2019-11-25 21:44:14 +0800

    基础学习

---
+++ 002基础/practise01.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+n = 123
+f = 456.789
+s1 = 'hello,world'
+s2 = 'hello,\'Adam\''
+s3 = r'hello,"Bart"'
+s4 = r'''hello,
+Lisa'''
+
+print(n,f,s1,s2,s3,s4)
+#print(n,f,s1,s2,s3,s4,sep='\n')
+#print("%s\n%s\n%s1\n%s2\n%s3\n%4" %(n, f, s1, s2, s3, s4))
+#print('n =',n,'\nf =',f,'\ns1 =',s1,'\ns2 =',s2,'\ns3 =',s3,'\ns4 =',s4)
