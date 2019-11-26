4d27108d54c350eeb6fe54ea1528e4485e9ee40c
Chao Ming <sxmc1994@163.com>
2019-11-26 16:00:48 +0800

    函数学习第一次

---
+++ 003函数/practise01.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+n1 = 255
+n2 = 1000
+#a = hex(n1)
+#b = hex(n2)
+#print(str(hex(n1)))
+#print(hex(n2))
+print('n1和n2的十六进制字符串是：\n%-16s，%-16s'%(hex(n1),hex(n2)))
+#print('n1和n2的十六进制字符串是：\n%s'%a,'\n%s'%b)
---
+++ 003函数/test03.py
@@ -0,0 +1,6 @@
+#!/usr/bin/evn python3
+# -*- coding: utf-8 -*-
+
+from test02 import my_abs
+
+print(my_abs(-9))
